6b4a5a10477c2879ab1cdd8e38a843a69257bf12
Nesto <danzer38@gmail.com>
2022-04-06 18:33:38 +0200

    Updated Settings Design

---
+++ IconViewer.View/Resources/Converter/InvertedBooleanToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace IconViewer.View.Resources.Converter
+{
+    internal class InvertedBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+
+            return (bool)value ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
